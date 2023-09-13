06b25a71ce79bc4da9328b19193295b138071187
chandukeertipati <131877124+chandukeertipati@users.noreply.github.com>
2023-09-13 11:15:17 +0530

    New changes

--- DotNetDemo/src/app/expense-input/expense-input.component.ts
+++ DotNetDemo/src/app/expense-input/expense-input.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { ExpenseServiceService } from '../expense-service.service';
 import {Expense}  from '../expense-input/expense-modal.component'
+import { Router } from '@angular/router';
 @Component({
   selector: 'app-expense-input',
   templateUrl: './expense-input.component.html',
@@ -13,7 +14,7 @@ export class ExpenseInputComponent implements OnInit {
   
   expense: Expense = new Expense();
 
-  constructor(private expenseService: ExpenseServiceService) {}
+  constructor(private expenseService: ExpenseServiceService,private router:Router) {}
   ngOnInit(): void {
   }
   onSubmit() {
@@ -21,6 +22,9 @@ export class ExpenseInputComponent implements OnInit {
       // Clear the form fields after adding an expense
       this.expense = new Expense();
     });
+  }
+  goBack() {
+    this.router.navigate(['/Navbar']); // Replace '/input' with the route to your input component
   }
 
 
--- DotNetDemo/src/app/expense-report/expense-report.component.ts
+++ DotNetDemo/src/app/expense-report/expense-report.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { ExpenseServiceService } from '../expense-service.service';
 import {Expense}  from '../expense-input/expense-modal.component'
+import { Router } from '@angular/router';
 @Component({
   selector: 'app-expense-report',
   templateUrl: './expense-report.component.html',
@@ -10,7 +11,7 @@ export class ExpenseReportComponent implements OnInit {
 
   expenses: Expense[] = [];
 
-  constructor(private expenseService: ExpenseServiceService) {}
+  constructor(private expenseService: ExpenseServiceService,private router:Router) {}
 
   ngOnInit() {
     this.getExpenses();
@@ -20,6 +21,9 @@ export class ExpenseReportComponent implements OnInit {
     this.expenseService.getExpenses().subscribe((expenses) => {
       this.expenses = expenses as Expense[];
     });
+  }
+  goBack() {
+    this.router.navigate(['/Navbar']); // Replace '/input' with the route to your input component
   }
 
 }
